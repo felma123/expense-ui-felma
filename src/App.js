80a8848561ebfa4d11d7abd7301df769aa0fc6cf
Joan <jmsinena@gmail.com>
2024-02-18 08:39:09 +0800

    add saving expenses

--- src/App.js
+++ src/App.js
@@ -1,50 +1,100 @@
 
 import { useEffect, useState } from "react";
 function renderExpenses(expenses) {
-     const rows = expenses.map((expense) => {
-       return (
-         <tr key={expense._id}>
-           <td>{expense._id}</td>
-           <td>{expense.description}</td>
-           <td>{expense.amount}</td>
-          <td>{new Date(expense.date).toDateString()}</td>
-        </tr>
-       );
-    });
-  
-    return rows;
-   }
-  
+  const rows = expenses.map((expense) => {
+    return (
+      <tr key={expense._id}>
+        <td>{expense._id}</td>
+        <td>{expense.description}</td>
+        <td>{expense.amount}</td>
+        <td>{new Date(expense.date).toDateString()}</td>
+      </tr>
+    );
+  });
+
+  return rows;
+}
+
 
 function App() {
   const [expenses, setExpenses] = useState([]);
 
-    const fetchExpenses = async () => {
-        const apiUrl = "https://workshop-expenses-apifelma.onrender.com";
-    
-        const endpoint = `${apiUrl}/api/expenses`;
-    
-        const response = await fetch(endpoint);
-    
-        const expenseData = await response.json();
-    
-        setExpenses(expenseData);
-      };
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState(
+    new Date(Date.now()).toLocaleDateString("en-CA")
+  );
+  const [onSuccessfulSave, setOnSuccessfulSave] = useState(false);
 
-        useEffect(() => {
-            fetchExpenses();
-          }, []);
-        
+  const fetchExpenses = async () => {
+    const apiUrl = "https://workshop-expenses-apifelma.onrender.com";
+
+    const endpoint = `${apiUrl}/api/expenses`;
+
+    const response = await fetch(endpoint);
+
+    const expenseData = await response.json();
+
+    setExpenses(expenseData);
+  };
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  const saveExpense = async (event) => {
+    event.preventDefault();
+
+    const apiUrl = "https://workshop-expenses-apifelma.onrender.com";
+
+    const endpoint = `${apiUrl}/api/expenses`;
+
+    const expense = {
+      description: description,
+      amount: amount,
+      date: date,
+    };
+
+    await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(expense),
+    });
+
+    setOnSuccessfulSave(true);
+  };
+
+  useEffect(() => {
+    if (onSuccessfulSave) {
+      fetchExpenses();
+    }
+  }, [onSuccessfulSave])
+
   return (
     <div>
-      <form>
-        <textarea cols="30" rows="10"></textarea>
-        <input type="number" />
-        <input type="date" />
+      <form onSubmit={saveExpense}>
+        <textarea
+          cols="30"
+          rows="10"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        ></textarea>
+        <input
+          type="number"
+          value={amount}
+          onChange={(event) => setAmount(event.target.value)}
+        />
+        <input
+          type="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
         <button>Save</button>
       </form>
 
-            <h2>My Expenses</h2>
+      <h2>My Expenses</h2>
 
       <table width="100%">
         <thead>
@@ -57,7 +107,7 @@ function App() {
         </thead>
 
         <tbody>
-        {renderExpenses(expenses)}
+          {renderExpenses(expenses)}
         </tbody>
       </table>
     </div>
